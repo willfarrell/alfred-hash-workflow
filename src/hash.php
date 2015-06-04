a3306c6b86b6b4dccd2c19e425b196d5f0f08fa5
Matt Cantillon <matthew.cantillon@gmail.com>
2015-06-04 19:47:49 +1000

    Added basic support for BCrypt hashing

--- src/hash.php
+++ src/hash.php
@@ -14,19 +14,22 @@ if (strpos($query, " ") !== false) {
 	$parts = explode(" ", $query);
 	$algo_q = array_shift($parts);
 	$string = implode($parts);
-	
-	foreach($algos as $algo) {
-		$pos = strpos($algo, $algo_q);
-		if ($pos !== false && $pos == 0) {
-			$hash = hash($algo, $string);
-			//echo "hash-$algo", $hash, "$algo", $hash, 'icon.png', 'yes\n';
-			$w->result( "hash-$algo", $hash, "$algo", $hash, 'icon.png', 'yes' );
+	if($algo_q == "bcrypt"){
+		$hash = password_hash($string, PASSWORD_DEFAULT);
+		$w->result("bcrypt", $hash, "bcrypt", $hash, 'icon.png', 'yes');
+	} else {
+		foreach($algos as $algo) {
+			$pos = strpos($algo, $algo_q);
+			if ($pos !== false && $pos == 0) {
+				$hash = hash($algo, $string);
+				//echo "hash-$algo", $hash, "$algo", $hash, 'icon.png', 'yes\n';
+				$w->result( "hash-$algo", $hash, "$algo", $hash, 'icon.png', 'yes' );
+			}
 		}
 	}
-	
 }
 
-if ( count( $w->results() ) == 0 ) {
+if ( count( $w->results() ) == 0 ) {	
 	foreach($algos as $algo) {
 		$hash = hash($algo, $query);
 		$w->result( "hash-$algo", $hash, "$algo", $hash, 'icon.png', 'yes' );
